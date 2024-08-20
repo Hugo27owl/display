6433302d239fc4e2e17062b5fc9204e3fd615e1f
hugo <hugoewhbaxter27@gmail.com>
2024-08-20 20:57:15 +0000

    Home UI build 1

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('brightness').addEventListener('click', function() {
+  alert('Brightness slider clicked');
+});
+
+document.getElementById('volume').addEventListener('click', function() {
+  alert('Volume slider clicked');
+});
